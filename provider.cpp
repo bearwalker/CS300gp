60190585bd7d7ac2114c429e494bd73b7014b8ff
Aelric Riggs <aelric@pdx.edu>
2020-02-23 15:25:44 -0800

    actually deleted overlapping files and added more comparison overloads

--- provider.cpp
+++ provider.cpp
@@ -1,8 +1,57 @@
-#include "defs.h"
-#include "provider.h"
+#include <fstream>
+#include <string>
 
-Provider(std::string informationFile);
-bool checkServiceID(unsigned int serviceId);
-void printServiceList();
-void createSessionReport(Member member, Service service, std::chrono::time_point time);
-void summaryReport();
+#include "Provider.h"
+
+Provider::Provider() {
+	id = 0;
+	name = "";
+	address = "";
+	city = "";
+	state = "";
+	zip = 0;
+}
+
+Provider::Provider(std::sring informationFile) {
+	// If the information file can't be read use null defaults
+	if (!loadInformation(informationFile)) {
+			id = 0;
+			name = "";
+			address = "";
+			city = "";
+			state = "";
+			zip = 0;
+	}
+}
+
+bool Provider::loadInformation(std::string informationFile) {
+	// 
+	std::ifstream;
+}
+
+
+	
+
+bool Provider::checkServiceID(unsigned int serviceID) {
+	auto serviceIterator = servicesProvided.find(serviceID);
+
+	// Find returns an iterator to end() if it can't find it
+	if (serviceIterator == servicesProvided.end())
+		return false;
+	else
+		return true;
+}
+
+
+bool operator<(const Provider& left_side, const Provider& right_side) {
+	return left_side.id < right_side.id;
+}
+
+bool operator<(const unsigned int& left_side, const Provider& right_side) {
+	return left_side < right_side.id;
+}
+
+bool operator<(const Provider& left_side, const unsigned int& right_side) {
+	return left_side.id < right_side;
+}
+
--- provider.h
+++ provider.h
@@ -13,8 +13,11 @@ public:
 	void printServiceList();
 	void createSessionReport(Member member, Service service, std::chrono::time_point time);
 	void summaryReport();
+	// For sorting a std::set of Providers
 	friend bool operator<(const Provider& left_side, const Provider& right_side);
-	friend bool operator==(const Provider& left_side, const Provider& right_side);
+	// For finding a Provider in a set based on its ID
+	friend bool operator<(const unsigned int& left_side, const Provider& right_side);
+	friend bool operator<(const Provider& left_side, const unsigned int& right_side);
 
 private:
 	unsigned int id;
