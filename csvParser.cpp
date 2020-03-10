9dc703af4d9f20d0fd0c6ddda5c9615dc3e654e2
Aelric Riggs <aelric@pdx.edu>
2020-03-09 19:40:18 -0700

    added catch2 header and some work on csvTests

--- csvParser.cpp
+++ csvParser.cpp
@@ -4,7 +4,7 @@
 #include <tuple>
 #include <vector>
 
-#include "defs.h"
+#include "csvParser.h"
 
 template <typename... Types>
 bool parseFile(std::vector<std::tuple<Types...>>& fileContents, std::string filePath, char seperator, char terminator, char comment) {
