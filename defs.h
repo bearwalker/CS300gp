648e328087cfaad3f6789069eea3fde647c0e68d
Aelric Riggs <aelric@pdx.edu>
2020-03-13 16:21:30 -0700

    woops forgot defs.h

--- defs.h
+++ defs.h
@@ -128,30 +128,185 @@ private:
 //Def of member class
 class Member
 {
-	public:
-		Member(); //initializes members to 0
-		bool printMemberInfo(); //Searches for member based on ID number.  Retruns false if member not found in list.
-		void setMemberInfo(); //sets member info from user. Refactor to read in from file.
-		bool getMemStatus();
-		void memberInactive();
-		bool printMemServices();
-		int addService(Service);
+public:
+	// Construct with empty values
+	Member();
 
-		//these operators need to overloaded in order to use set
-		//for sorting a set of Members
-		friend bool operator<(const Member& left_side, const Member& right_side);
-		//for finding a Member in a set based on its ID
-		friend bool operator<(const unsigned int& left_side, const Member& right_side);
-		friend bool operator<(const Member& left_side, const unsigned int& right_side);
-	private:
-		unsigned int ID_number;
-		bool Mem_status;
-		std::string name;
-		std::string address;
-		std::string city;
-		std::string state;
-		int zip;
-		std::set<Service, std::less<>> servicesUsed;
+	/*
+	  Try to construct the class with data from a csv file, otherwise use empty values.
+	  A member csv file should be formatted as id,name,address,city,state,zip,status;
+	  
+	  @param informationFile - path to a csv file containing provider info
+	*/
+	Member(std::string informationFile);
+
+	/*
+	  Loads member information from a csv file formatted like so:
+	  id,name,address,city,state,zip,status;
+
+	  @param informationFile - path to a csv file containing member info
+
+	  @return true on success, false on failure
+	*/
+	bool loadInformation(std::string informationFile);
+
+	/*
+	  Saves a session into sessions and writes to members session data directory
+	  Typical usage would be to save a session returned by Provider::saveSessionReport()
+
+	  @param session - the session to save
+	*/
+	void saveSession(Session session);
+
+	/*
+	  Outputs a report containing the following information for the last 7 days to the given stream:
+	  - Member name 25 characters
+	  - Member number 9 digits
+	  - Member street address 25 characters
+	  - Member city 14 characters
+	  - Member state 2 characters
+	  - Member zip code 5 digits
+	  - Data for each service used
+	  	- Date of service MM-DD-YYYY
+		- Provider name 25 characters
+		- Service name 20 characters
+
+	  @param out - the ostream to output he report to
+	*/
+	void weekReport(std::ostream& out);
+
+	/*
+	  Prints weekReport to std::cout
+	*/
+	void printWeekReport();
+
+	/*
+	  Prints weekReport to the given file
+
+	  @param filePath - path to the file to save the report in
+
+	  @return true if file was opened, false if it couldn't be
+	*/
+	bool saveWeekReport(std::string filePath);
+
+	/*
+	  Sets id to the given unsigned int, cutting it down to the first ID_DIGITS digits if too large
+	  or padding it with 0's if too small
+
+	  @param newID - the number to set ID to
+	*/
+	void setID(unsigned int newID);
+
+	/*
+	  Sets name to the given string, cutting it down to NAME_CHARACTERS if too large
+
+	  @param newName - the string to set name to
+	*/
+	void setName(std::string newName);
+
+	/*
+	  Sets address to the given string, cutting it down to ADDRESS_CHARACTERS if too large
+
+	  @param newAddress - the string to set address to
+	*/
+	void setAddress(std::string newAddress);
+
+	/*
+	  Sets city to the given string, cutting it down to CITY_CHARACTERS if too large
+
+	  @param newCity - the string to set city to
+	*/
+	void setCity(std::string newCity);
+
+	/*
+	  Sets state to the given string, cutting it down to STATE_CHARACTERS if too large
+
+	  @param newState - the string to set state to
+	*/
+	void setState(std::string newState);
+
+	/*
+	  Sets zip to the given unsigned int, cutting it down to ZIP_DIGITS if too large or padding with
+	  0's if too small
+
+	  @param newZip - the number to set zip to
+	*/
+	void setZip(unsigned int newZip);
+
+	/*
+	  Sets the members status. False indicates suspended, true indicates valid/not suspended.
+
+	  @param newStatus - the members new status
+	*/
+	void setStatus(bool newStatus);
+
+	/*
+	  Getter for ID
+	  
+	  @return the memberss ID
+	*/
+	unsigned int getID() const;
+
+	/*
+	  Getter for name
+
+	  @return the members name
+	*/
+	std::string getName() const;
+
+	/*
+	  Getter for address
+
+	  @return the members address
+	*/
+	std::string getAddress() const;
+
+	/*
+	  Getter for city
+
+	  @return the city the member lives in
+	*/
+	std::string getCity() const;
+
+	/*
+	  Getter for state
+
+	  @return the state the member lives in
+	*/
+	std::string getState() const;
+
+	/*
+	  Getter for zip
+
+	  @return the members zip code
+	*/
+	unsigned int getZip() const;
+
+	/*
+	  Getter for status
+
+	  @return the members status, false indicates suspended, true indicates not suspended/valid
+	*/
+	bool getStatus() const;
+	
+
+	bool printMemberInfo(); //Searches for member based on ID number.  Retruns false if member not found in list.
+	//these operators need to overloaded in order to use set
+	//for sorting a set of Members
+	friend bool operator<(const Member& left_side, const Member& right_side);
+	//for finding a Member in a set based on its ID
+	friend bool operator<(const unsigned int& left_side, const Member& right_side);
+	friend bool operator<(const Member& left_side, const unsigned int& right_side);
+	
+private:
+	unsigned int id;
+	std::string name;
+	std::string address;
+	std::string city;
+	std::string state;
+	unsigned int zip;
+	bool status;
+	std::vector<Session> sessions;
 };
 
 // Def of Session class
@@ -201,6 +356,13 @@ public:
 	void setServiceProvided(Service service);
 
 	/*
+	  Setter for providerName
+
+	  @param name - the name of the provider that provided the service
+	*/
+	void setProviderName(std::string name);
+
+	/*
 	  Setter for dateProvided
 
 	  @param date - the date the service was provided
@@ -236,6 +398,13 @@ public:
 	Service getServiceProvided() const;
 
 	/*
+	  Getter for providerName
+
+	  @return name of the provider that provided the service
+	*/
+	std::string getProviderName() const;
+	
+	/*
 	  Getter for dateProvided
 
 	  @return a time_point of the date provided
@@ -259,6 +428,7 @@ public:
 private:
 	Member providedTo;
 	Service serviceProvided;
+	std::string providerName;
 	std::chrono::system_clock::time_point dateProvided;
 	std::chrono::system_clock::time_point timeRecorded;
 	std::string comments;
@@ -330,8 +500,10 @@ public:
 	  @param service - the service that was provided
 	  @param dateProvided - the date the service was provided on
 	  @param comments - any comments the provider wants to make on the session
+
+	  @return the session that was created and saved
 	*/
-	void saveSessionReport(Member member, Service& service, std::chrono::system_clock::time_point dateProvided, std::string comments);
+	Session saveSessionReport(Member member, Service service, std::chrono::system_clock::time_point dateProvided, std::string comments);
 
 	/*
 	  Outputs a report containing the following information for the last 7 days to the given stream:
@@ -353,7 +525,7 @@ public:
 
 	  @param out - the ostream to output the report to
 	 */
-	void weekReport();
+	void weekReport(std::ostream& out);
 
 	/*
 	  Prints weekReport to std::cout
