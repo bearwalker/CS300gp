2f31aeebc354b7fdd0ad03785d7dff8ab9b3dd7b
Walker Bear <kwb3@augusta.cs.pdx.edu>
2020-03-13 19:25:16 -0700

    fixed filesystem error in member and provider

--- member.cpp
+++ member.cpp
@@ -2,11 +2,12 @@
 
 #include <chrono>
 #include <ctime>
-#include <filesystem>
 #include <string>
 #include <tuple>
 #include <vector>
 #include <iostream>
+#include <iomanip>
+#include <experimental/filesystem>
 
 #include "member.h"
 #include "defs.h"
@@ -57,7 +58,7 @@ bool Member::loadInformation(std::string informationFile)
 	setStatus(std::get<6>(information.at(0)));
 
 	// Load session records
-	for (auto file: std::filesystem::directory_iterator("MEMBER_DATA_DIR" + std::to_string(id) + SESSION_DATA_SUBDIR)) {
+	for (auto file: std::experimental::filesystem::directory_iterator("MEMBER_DATA_DIR" + std::to_string(id) + SESSION_DATA_SUBDIR)) {
 		Session newSession;
 		if (newSession.loadInformation(file.path()))
 			sessions.push_back(newSession);
